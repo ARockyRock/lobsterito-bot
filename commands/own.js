7aa997deca29eb5d5807f3a782a32e12980effe6
Elephysics <mattptrs123@gmail.com>
2021-03-12 21:36:48 -0500

    Added ownage leaderboard arg
    Better not misspell leaderboard

--- commands/own.js
+++ commands/own.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const uno_reverse = new Discord.MessageAttachment('./imgs/uno_reverse.png');
+fs = require('fs');
+
 module.exports = {
 	name: 'own',
 	alias: ['owned'],
@@ -7,27 +9,56 @@ module.exports = {
 	cooldown: 1800,
 	usage: '@[user]',
 	guildOnly: true,
-	execute(msg, args) {
-		if (!msg.mentions.users.size) return msg.reply('I owned your mother last night.');
+	execute(msg, args, client) {
+		args=args.shift().toLowerCase();
 
-		const taggedUser = msg.mentions.members.first();
-		const sender = msg.member;
+		if (args != 'leaderboard'){
+			if (!msg.mentions.users.size) return msg.reply('I owned your mother last night.');
 
-		if (taggedUser.voice.channelID == null) {
-			msg.reply('can\'t own a user not in voice.');
-		} else {
-			if ((Math.floor(Math.random() * 100)) < 2) {
-				// Random chance (2%) the own command is reversed
-				msg.channel.send(uno_reverse);
-				sender.voice.setChannel('406611345887461377')
-				.then(() => msg.channel.send(`<@${sender.id}> has been owned by <@${taggedUser.id}>.`))
-				.catch(console.error);
+			const taggedUser = msg.mentions.members.first();
+			const sender = msg.member;
+
+			if (taggedUser.voice.channelID == null) {
+				msg.reply('can\'t own a user not in voice.');
 			} else {
-				//Own command functions normally
-				taggedUser.voice.setChannel('406611345887461377')
-				.then(() => msg.channel.send(`<@${taggedUser.id}> has been owned by <@${sender.id}>.`))
-				.catch(console.error);
+				if ((Math.floor(Math.random() * 100)) < 2) {
+					// Random chance (2%) the own command is reversed
+					msg.channel.send(uno_reverse);
+					sender.voice.setChannel('747241493789409391')
+					.then(() => msg.channel.send(`<@${sender.id}> has been owned by <@${taggedUser.id}>.`))
+					.catch(console.error);
+				} else {
+					//Own command functions normally
+					taggedUser.voice.setChannel('747241493789409391')
+					.then(() => msg.channel.send(`<@${taggedUser.id}> has been owned by <@${sender.id}>.`))
+					.catch(console.error);
+				}
 			}
 		}
+		else{
+
+			let OwnedL = JSON.parse(fs.readFileSync('./OwnedLeaderboard.json', 'utf-8')) //Reads the JSON wach time
+
+			OwnedL.sort(function(a, b){				//Sorts the array from highest to least
+   		 		return b.OwnedAmount - a.OwnedAmount;
+			})
+
+			var OwnedLB =[];
+			for(var i = 0; i < OwnedL.length; i++){		//Creates the leaderboard by going through the sorted array and getting usernames and amount owned
+ 				var UserN = client.users.cache.get(OwnedL[i].UserID); //Gets usernames
+				OwnedLB = OwnedLB.concat([UserN.username, OwnedL[i].OwnedAmount].join(': ')); //Makes array of leaderboard as Name: #
+			}
+
+			const serverInfoEmbed = new Discord.MessageEmbed() //Makes the leaderboard into a fancy embeded message.
+			// #36393e bg blend
+				.setColor('#b62827')
+				.setThumbnail(msg.guild.iconURL())
+				.setTitle('Owned Leaderboard')
+				.setDescription(OwnedLB)
+				.setTimestamp()
+				.setFooter(`${msg.guild.name}`);
+			msg.channel.send(serverInfoEmbed);
+		}
+
 	},
 };
