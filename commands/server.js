4672fff77b6e28b2da9c9b2b121a8f5c4fddc70d
Elephysics <mattptrs123@gmail.com>
2021-12-05 22:59:59 -0500

    Added number of emotes

--- commands/server.js
+++ commands/server.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 var NumRoles = 0;
+var NumEmojis = 0;
 
 module.exports = {
 	name: 'server',
@@ -8,7 +9,15 @@ module.exports = {
 	usage: '',
 	guildOnly: true,
 	execute(msg, args) {
-		const promise = new Promise((resolve, reject) => {
+		const PromiseEmojis = new Promise((resolve, reject) => {
+			msg.guild.emojis.fetch()
+			.then(emojis =>{
+				NumEmojis = emojis.size;
+				resolve(NumEmojis);
+			})
+			.catch(console.error)
+		})
+		const PromiseRoles = new Promise((resolve, reject) => {
 			msg.guild.roles.fetch()
 			.then(roles =>{
 				NumRoles = roles.size;
@@ -16,20 +25,22 @@ module.exports = {
 			})
 			.catch(console.error)
 		})
-		promise.then(res =>{
-		const serverInfoEmbed = new Discord.MessageEmbed()
-		// #36393e bg blend
-			.setColor('#b62827')
-			.setThumbnail(msg.guild.iconURL())
-			.setTitle('Server Information')
-			.addField('Server Name', `${msg.guild.name}`)
-			.addField('Total Members', `${msg.guild.memberCount}`)
-			.addField('Total Number of Roles', `${NumRoles}`)
-			.addField('Server Created', `${msg.guild.createdAt}`)
-			.setTimestamp()
-			.setFooter(`${msg.guild.name}`);
+		Promise.all([PromiseRoles, PromiseEmojis]).then(res =>{
+			console.log(NumEmojis)
+			const serverInfoEmbed = new Discord.MessageEmbed()
+			// #36393e bg blend
+				.setColor('#b62827')
+				.setThumbnail(msg.guild.iconURL())
+				.setTitle('Server Information')
+				.addField('Server Name', `${msg.guild.name}`)
+				.addField('Total Members', `${msg.guild.memberCount}`)
+				.addField('Total Number of Roles', `${NumRoles}`)
+				.addField('Total Number of Emojis', `${NumEmojis}`)
+				.addField('Server Created', `${msg.guild.createdAt}`)
+				.setTimestamp()
+				.setFooter(`${msg.guild.name}`);
 
-		msg.channel.send({ embeds: [serverInfoEmbed]});
-	})
+			msg.channel.send({ embeds: [serverInfoEmbed]});
+		})
 	},
 };
